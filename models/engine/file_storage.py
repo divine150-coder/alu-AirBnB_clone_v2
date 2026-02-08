9dc20037384b2152c1ad3246a1b50201bf9eeea4
divinecoder-150 <d.mutesi1@alustudent.com>
2026-02-08 00:59:58 -0800

    add changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,41 +1,31 @@
 #!/usr/bin/python3
+"""FileStorage module for AirBnB clone"""
+
 import json
 from models.base_model import BaseModel
 from models.state import State
 from models.place import Place
 
-
-classes = {
-    "BaseModel": BaseModel,
-    "State": State,
-    "Place": Place
-}
-
-
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
-        return FileStorage.__objects
+    def all(self, cls=None):
+        if cls:
+            return {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
+        return self.__objects
 
     def new(self, obj):
         key = f"{obj.__class__.__name__}.{obj.id}"
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
-        with open(FileStorage.__file_path, 'w') as f:
-            json.dump(
-                {k: v.to_dict() for k, v in FileStorage.__objects.items()},
-                f
-            )
+        with open(self.__file_path, "w") as f:
+            json.dump({k: v.to_dict() for k, v in self.__objects.items()}, f)
 
-    def reload(self):
-        try:
-            with open(FileStorage.__file_path, 'r') as f:
-                objs = json.load(f)
-            for k, v in objs.items():
-                cls_name = v["__class__"]
-                self.new(classes[cls_name](**v))
-        except FileNotFoundError:
-            pass
+    def delete(self, obj=None):
+        if obj is None:
+            return
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        if key in self.__objects:
+            del self.__objects[key]
